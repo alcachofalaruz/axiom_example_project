53d450510699aeec09477e8a594f967c7a55df85
Alcachofalaruz <53383939+alcachofalaruz@users.noreply.github.com>
2026-09-10 22:01:35 -0300

    Add empty Odin entry point and executable build

--- build.sh
+++ build.sh
@@ -11,3 +11,13 @@ MINGW*|MSYS*|CYGWIN*) executable_suffix=.exe ;;
 esac
 
 "./axiom/build/axiom-metagen${executable_suffix}" src src game
+
+source ./axiom/tools/odin_env.sh
+odin_options=(-debug)
+if [[ "${1:-debug}" == release ]]; then
+    odin_options=(-o:speed)
+fi
+
+mkdir -p build
+"$odin_command" build src/main.odin -file "${odin_options[@]}" \
+    "-out:build/axiom_example_project${executable_suffix}"
